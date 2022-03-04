5e0c783da102d66c48d0998f5ce03a82a26bc8d5
Sudip Modi <sudipinshunya@gmail.com>
2022-03-05 03:25:43 +0530

    basic video call,first commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+Steps to use:
+
+1. clone the project
+2. navigate to zoom-clone
+3. run peerjs server using command "peerjs --port 9000"
+4. open another terminal and go to zoom-clone , then start the app using "nodemon server.js"
+5. Open new tab in the browser, and paste the url from the first tab
---
+++ server.js
@@ -0,0 +1,31 @@
+console.log("server code running");
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuidV4 } = require("uuid");
+const io = require("socket.io")(server);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  console.log(socket);
+  socket.on("join-room", (roomId, userId) => {
+    console.log(roomId, userId);
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+    socket.on("disconnect", () => {
+      socket.to(roomId).emit("user-disconnected", userId);
+    });
+  });
+});
+
+server.listen(3000);
